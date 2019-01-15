151164b5d7bcaf79b5dc0716091d006b5fac6a98
Maki <mxmcube@gmail.com>
2019-01-15 14:57:28 -0800

    New new tab

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-// Chrome gave me an error when I put this in the html file:
-//
-// Refused to execute inline script because it violates the following Content Security Policy directive:
-// "script-src 'self' blob: filesystem: chrome-extension-resource:".
-//
-// That's dumb. :/
+function tryPlaying() {
+	document.querySelector("video").play().then(()=>{}).catch(err=>{
+		console.log(err);
+		tryPlaying();
+	});
+}
 
-var img = ["boat","busy_train","calm_train","deer","fish_pond","fish_shop","girls","girls_waiting","home","pets","rainbow_dog","river","sakura","shore","take_away"];
-var rnd = Math.floor(Math.random()*img.length);
-document.querySelector("#bg").style.backgroundImage = "url(gifs/"+img[rnd]+".gif)";
-
-// Animated logo
-
-new MakiLogo(document.getElementById("logo")).intro();
+tryPlaying();
